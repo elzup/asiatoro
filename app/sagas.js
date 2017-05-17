7c0a75856ddebd9f83b8b47e0ddf0b3d26d6b080
elzup <guild0105@gmail.com>
2017-05-17 16:07:08 +0900

    Native ios android branch

--- app/sagas.js
+++ app/sagas.js
@@ -6,13 +6,17 @@ import types from "./constants"
 import { setAccessPoints } from "./action"
 import { NativeModules, Platform } from "react-native"
 
-const { ReadAccessPoint } = NativeModules
+const { ReadAccessPoint, NativeUtilModuleAndroid } = NativeModules
 
 function* fetchAccessPoint() {
 	let accessPoints = ["none"]
 	if (Platform.OS === "ios") {
 		const res = yield call(ReadAccessPoint.getAccessPoints)
 		accessPoints = JSON.parse(res)
+		accessPoints.push("in iOS")
+	} else {
+		console.log(NativeUtilModuleAndroid)
+		accessPoints.push("in Android")
 	}
 	console.log(accessPoints)
 	yield put(setAccessPoints(fromJS(accessPoints)))
