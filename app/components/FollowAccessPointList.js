d91ba3376143106d02533b158c76ea0896025bd5
elzup <guild0105@gmail.com>
2017-05-19 20:00:52 +0900

    People style

--- app/components/FollowAccessPointList.js
+++ app/components/FollowAccessPointList.js
@@ -1,8 +1,8 @@
 // @flow
 
 import React from "react"
-import { Content, Card, CardItem, Icon, Right } from "native-base"
-import { Text } from "react-native"
+import { Content, Card, CardItem, Icon, Col, Grid } from "native-base"
+import { View, Text } from "react-native"
 import { AccessPointRecord, CheckinRecord } from "../types"
 
 type Props = {
@@ -17,9 +17,18 @@ export default class FollowAccessPointList extends React.Component {
 	renderCheckinCardItem(ci: CheckinRecord) {
 		console.log(ci)
 		return (
-			<CardItem key={ci.user.name}>
+			<View
+				style={{
+					width: 70,
+					borderRadius: 3,
+					margin: 2,
+					padding: 2,
+					alignItems: "center",
+				}}
+			>
+				<Icon name="person" />
 				<Text>{ci.user.name}</Text>
-			</CardItem>
+			</View>
 		)
 	}
 
@@ -28,9 +37,14 @@ export default class FollowAccessPointList extends React.Component {
 		return (
 			<Card key={ap.bssid}>
 				<CardItem header>
+					<Icon name="wifi" />
 					<Text>{ap.ssid}({ap.bssid})</Text>
 				</CardItem>
-				{ap.checkins.map(ci => this.renderCheckinCardItem(ci))}
+				<View
+					style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+				>
+					{ap.checkins.map(ci => this.renderCheckinCardItem(ci))}
+				</View>
 			</Card>
 		)
 	}
