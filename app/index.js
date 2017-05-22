7f06426bb6c4121018eaaed64a32af021683519a
elzup <guild0105@gmail.com>
2017-05-22 18:46:51 +0900

    Setup remote-redux-devtools

--- app/index.js
+++ app/index.js
@@ -4,13 +4,17 @@ import React, { Component } from "react"
 import { createStore, applyMiddleware } from "redux"
 import createSagaMiddleware from "redux-saga"
 import { Provider } from "react-redux"
+import { composeWithDevTools } from "remote-redux-devtools"
 
 import AppContainer from "./containers/AppContainer"
 import reducer from "./reducer"
 import sagas from "./sagas"
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const store = createStore(
+	reducer,
+	composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
 
 sagaMiddleware.run(sagas)
 
