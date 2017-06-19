29b9f42fd0173843ff9136d731775d4c4df89b7e
elzup <guild0105@gmail.com>
2017-06-19 22:36:02 +0900

    Dummy data fro debug

--- app/natives/NetworkUtil/index.js
+++ app/natives/NetworkUtil/index.js
@@ -6,12 +6,16 @@ import {AccessPointRecord} from "../../types"
 const {ReadAccessPoint, NativeUtilModuleAndroid} = NativeModules
 
 export function* getAccessPoints(): Array<AccessPointRecord> {
-	if (false && __DEV__) {
+	if (__DEV__) {
 		return [
-			new AccessPointRecord({ssid: "Hoge", power: -50, bssid: "aa:bb:cc:d1"}),
-			new AccessPointRecord({ssid: "1号館", power: -50, bssid: "bssid1"}),
-			new AccessPointRecord({ssid: "2号館", power: -50, bssid: "bssid2"}),
-			new AccessPointRecord({ssid: "3号館", power: -50, bssid: "bssid3"}),
+			new AccessPointRecord({
+				ssid: "dummy-Hoge",
+				power: -50,
+				bssid: "aa:bb:cc:d1",
+			}),
+			new AccessPointRecord({ssid: "dummy-1号館", power: -50, bssid: "bssid1"}),
+			new AccessPointRecord({ssid: "dummy-2号館", power: -50, bssid: "bssid2"}),
+			new AccessPointRecord({ssid: "dummy-3号館", power: -50, bssid: "bssid3"}),
 		]
 	}
 	if (Platform.OS === "ios") {
